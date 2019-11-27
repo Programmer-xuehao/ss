daeebcab166a4d5f440ce8e95e6b286074a825d4
方卓 <fangzhuo@ifeng.com>
2019-11-27 14:24:26 +0800

    update

--- src/main/java/com/ifeng/mcn/common/base/TaskStatusEnum.java
+++ src/main/java/com/ifeng/mcn/common/base/TaskStatusEnum.java
@@ -9,10 +9,34 @@ package com.ifeng.mcn.common.base;
  */
 public enum TaskStatusEnum {
 
+    /**
+     * 激活
+     */
     ACTIVE("1"),
+    /**
+     * 初始化
+     */
     INIT("0"),
+    /**
+     * 暂停抓取
+     */
     PAUSE("2"),
+    /**
+     * 账号异常 注销
+     */
     ACCOUNT("3"),
+    /**
+     * 停止更新  不更新  没有内容  半年以上
+     */
+    NOUPDATE("4"),
+    /**
+     * 错误链接
+     */
+    ERRORLINK("5"),
+    /**
+     * 没有数据
+     */
+    NODATA("6")
     ;
     private String  code  ;
 
